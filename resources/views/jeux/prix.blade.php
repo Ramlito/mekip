b13d8c05126ac1863c4c69bf3890abe7d93d6882
benjamin <benjamin.kubik@env.univ-artois.fr>
2020-12-17 18:52:13 +0100

    ajout de quelques couleurs

--- resources/views/jeux/prix.blade.php
+++ resources/views/jeux/prix.blade.php
@@ -1,4 +1,3 @@
-<html>
 <head>
     <title>Statistiques du jeu {{$jeu->nom}}</title>
 </head>
@@ -10,9 +9,27 @@
 <h2>Nombre Joueurs connectés :{{$jeu -> nombre_joueurs}}</h2>
 <h2>Nombre d'achats :{{$nbJoueurs}}</h2>
 <br>
-<h2>Note Moyenne : {{$noteMoyenne}}</h2>
-<h2>Note maximale : {{$noteMax}}</h2>
-<h2>Note minimale : {{$noteMin}}</h2>
+@if($noteMin < 1.5)
+    <h2 style="color: darkred">Note minimum : {{$noteMin}}</h2>
+@elseif($noteMin < 3.5)
+    <h2 style="color: gold">Note minimum : {{$noteMin}}</h2>
+@else
+    <h2 style="color: limegreen">Note minimum : {{$noteMin}}</h2>
+@endif
+@if($noteMax < 1.5)
+    <h2 style="color: darkred">Note maximum : {{$noteMax}}</h2>
+@elseif($noteMax < 3.5)
+    <h2 style="color: gold">Note maximum : {{$noteMax}}</h2>
+@else
+    <h2 style="color: limegreen">Note maximum : {{$noteMax}}</h2>
+@endif
+@if($noteMoyenne < 1.5)
+    <h2 style="color: darkred">Note moyen : {{$noteMoyenne }}</h2>
+@elseif($noteMoyenne  < 3.5)
+    <h2 style="color: gold">Note moyen : {{$noteMoyenne }}</h2>
+@else
+    <h2 style="color: limegreen">Note moyen : {{$noteMoyenne }}</h2>
+@endif
 <h2>Nombre de commentaires : {{$nbCommentaires}}</h2>
 <h2>Nombre total de commentaires : {{$nbCommentairesTotal}}</h2>
 </body>
