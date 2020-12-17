628871352f723b0a65fef483e783b7e00de17273
benjamin <benjamin.kubik@env.univ-artois.fr>
2020-12-17 03:56:11 +0100

    Corrections des conflits

--- resources/views/user/collection.blade.php
+++ resources/views/user/collection.blade.php
@@ -10,11 +10,11 @@
         @foreach($collection as $jeu)
             <li>Nom du jeu : {{$jeu->nom}}, le thème : {{$jeu->theme->nom}}, la durée d'une partie : {{$jeu->duree}}, le nombre de joueurs : {{$jeu->nombre_joueurs}}
                 <a href="{{route('jeux.show',['id'=>$jeu->id])}}">détails</a></li>
+            @if(auth()->check())
+                <a href="{{route('user.suppression',$jeu->id)}}">supprimer</a>
+            @endif
         @endforeach
     </ul>
-    @if(auth()->check())
-        <a href="{{route('user.suppression',$jeu->id)}}">supprimer</a>
-    @endif
 @else
     <h3>aucun jeu</h3>
 @endif
